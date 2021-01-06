72ebc4f889fc33b7e5a7d20772a87d4972ada087
Katrina Dierking <kalynndier@gmail.com>
2021-01-05 22:47:20 -0500

    troubleshooting slide out menu. Getting errors

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+// import './Menu.css';
 import Home from "./components/staticPages/Home"
+
+
 
 function App() {
   return (
--- mobile/src/index.js
+++ mobile/src/index.js
@@ -7,7 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const styleLink = document.createElement("link");
 styleLink.rel = "stylesheet";
 styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+
 document.head.appendChild(styleLink);
+
 
 ReactDOM.render(
   <React.StrictMode>

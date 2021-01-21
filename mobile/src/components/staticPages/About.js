0446ee1368a34a200aeecc31898320fe244e3fe1
Katrina Dierking <kalynndier@gmail.com>
2021-01-20 23:10:09 -0500

    home route complete. Working on the about route and nav bar

--- mobile/src/components/staticPages/About.js
+++ mobile/src/components/staticPages/About.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactTooltip from 'react-tooltip'
-import Header from './main/Header'
-import NavBar from './main/NavBar'
-import Footer from './main/Footer'
+import Top from './main/Top'
+// import Header from './main/Header'
+// import NavBar from './main/NavBar'
+// import Footer from './main/Footer'
 
 export default function About () {
     return (
             <>
-            <Header />
+            <Top /> 
+            {/* <Header />
             <NavBar /> 
-            <Footer />
+            <Footer /> */}
             </>
     )
 }
--- mobile/src/index.js
+++ mobile/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from 'react-router-dom'
 
 const styleLink = document.createElement("link");
 styleLink.rel = "stylesheet";
@@ -13,7 +14,9 @@ document.head.appendChild(styleLink);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );

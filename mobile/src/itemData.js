e881fed52208d0285b1e8dc55cb8dec8b575cf40
Katrina Dierking <kalynndier@gmail.com>
2021-01-23 00:00:04 -0500

    item database started

---
+++ mobile/src/itemData.js
@@ -0,0 +1,43 @@
+import {v4 as uuid} from 'uuid'
+
+export const itemData = [
+    {
+        id: uuid(),
+        title: "JavaScript at a Glance",
+        img: "",
+        description: "",
+        price: $27
+    },
+
+    {
+        id: uuid(),
+        title: "HTML/CSS at a Glance",
+        img: "",
+        description: "",
+        price: $27
+    }, 
+
+    {
+        id: uuid(),
+        title: "JavaScript Workbook",
+        img: "",
+        description: "",
+        price: $47
+    }, 
+
+    {
+        id: uuid(),
+        title: "React Workbook",
+        img: "",
+        description: "",
+        price: $47
+    }, 
+
+    {
+        id: uuid(),
+        title: "React at a Glance",
+        img: "",
+        description: "",
+        price: $27
+    }
+]

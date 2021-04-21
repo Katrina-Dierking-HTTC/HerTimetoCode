f32255b3c59c607d9077fbb22ee27485a64194bd
Katrina Dierking <kalynndier@gmail.com>
2021-04-21 07:46:21 -0400

    About style and Button style refactored

---
+++ mobile/src/components/design/ButtonStyle.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+
+export const ButtonWrap = styled.div`
+    background-color: #17c436;
+    border-radius: 10%;
+    margin-left: 2vw;
+    max-width: 20vw;
+    padding:1.5%;
+    max-height: 4vh;
+
+    h3 {
+        font-size: .85rem;
+        color:white;
+        text-align:center;
+        padding: 1%;
+    }
+`
--- mobile/src/components/main/Button.js
+++ mobile/src/components/main/Button.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import ButtonWrap from '../design/ButtonStyle'
 
 
 export default function Button () {
@@ -9,19 +9,3 @@ export default function Button () {
         </ButtonWrap>
     )
 }
-
-const ButtonWrap = styled.div`
-    background-color: #17c436;
-    border-radius: 10%;
-    margin-left: 2vw;
-    max-width: 20vw;
-    padding:1.5%;
-    max-height: 4vh;
-
-    h3 {
-        font-size: .85rem;
-        color:white;
-        text-align:center;
-        padding: 1%;
-    }
-`
